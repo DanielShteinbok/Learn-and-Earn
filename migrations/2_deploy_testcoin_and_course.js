bfd642d2bf5bf35550ad2369d25c5034251e57cc
Daniel Shteinbok <dshteinbok@gmail.com>
2020-05-23 08:25:22 -0400

    updated builds

--- migrations/2_deploy_testcoin_and_course.js
+++ migrations/2_deploy_testcoin_and_course.js
@@ -3,6 +3,6 @@ const Course = artifacts.require("Course");
 
 module.exports = function(deployer) {
 	deployer.deploy(TestCoin).then(function() {
-		return deployer.deploy(Course, 10, 15, 2, TestCoin.address)
+		return deployer.deploy(Course, 10, 15, 2, TestCoin.address, TestCoin.address, "0x0000000000000000000000000000000000000000000000000000000000000000", TestCoin.address, TestCoin.address, TestCoin.address)
 	});
 };
